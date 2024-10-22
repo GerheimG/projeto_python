ee76449d4dc1545465a301397529a3a730378266
GerheimG <g.gerheimferro@gmail.com>
2024-10-22 16:38:23 -0300

    primeiro_codigo

---
+++ aula01_primeiro_programa/helloworld.py
@@ -0,0 +1,20 @@
+# Curso de Desenvolvimento de Sistemas
+# Turma: 0392
+# Autor: Guilherme Gerheim
+# Data : 22/10/2024
+# Primeiro Programa em Python
+
+# Importanto as bibliotecas 
+import os 
+
+
+#limpando terminal 
+os.system('cls')
+
+
+# O comando print() executa uma saida via terminal
+print('~' * 70)
+print('Vai Flamengo')
+print('Maior do Brasil')
+print('~' * 70)
+print()
---
+++ aula02_variaveis/variaveis.py
@@ -0,0 +1,21 @@
+import os
+
+os.system('cls')
+
+print('~' * 70)
+print('Estudo de Variáveis')
+print('~' * 70)
+
+nome = 'Jorge Silva'
+nascimento = 2005
+altura = 1.75
+peso = 82.7
+doador = True 
+complexo = 3j
+PI = 3.14
+
+
+print('\033[0m \033[31mTipos declarados:\033[0m')
+print('\033[0m A var \033[32m nome \033[0m é do tipo: ', type(nome))
+print('\033[0m A var \033[32m nascimento \033 [0m é do tipo: ', type(nascimento))
+print('\033[0m A var]')
