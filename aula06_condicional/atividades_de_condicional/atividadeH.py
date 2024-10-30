f647ddaaef5508c41cc5acab97fd1e8e5f18c064
GerheimG <g.gerheimferro@gmail.com>
2024-10-30 16:28:59 -0300

    atividade

--- aula06_condicional/atividades_de_condicional/atividadeH.py
+++ aula06_condicional/atividades_de_condicional/atividadeH.py
@@ -9,8 +9,8 @@ b = int(-6)
 c = int(5)
 
 delta = (b ** 2) - (4 * a * c)
-bhaskara1 = (-b + delta ** 0.5)  / 2*a
-bhaskara2 = (-b - delta ** 0.5)  / 2*a
+x1 = (-b + delta ** 0.5)  / 2*a
+x2 = (-b - delta ** 0.5)  / 2*a
 
 
-print(f'As raízes da equação são {bhaskara1, bhaskara2}')
+print(f'As raízes da equação são {x1, x2}')
