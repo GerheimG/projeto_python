7c0428386d3fb1526a4bbf0509bb2a2cb6a12245
GerheimG <g.gerheimferro@gmail.com>
2025-01-24 15:11:36 -0300

    atividade

--- aula13_funcoes/atividade/E.py
+++ aula13_funcoes/atividade/E.py
@@ -21,4 +21,16 @@ peso = float(input('Insira seu peso: '))
 # Invocando a função
 imc2 = calcular(altura, peso)
 
-print(f'Seu IMC: {imc2:.2f}')
+if imc2 < 18.5:
+    print(f'Seu imc {imc2:.2f}, Você está abaixo do peso')
+elif imc2 > 18.6 and imc2 < 24.9:
+    print(f'Seu imc {imc2:.2f}, Você está no peso ideal')
+elif imc2 > 25.0 and imc2 < 29.9:
+    print(f'Seu imc {imc2:.2f}, Você está levemente acima do peso')
+elif imc2 > 30.0 and imc2 < 34.9:
+    print(f'Seu imc {imc2:.2f}, Obesidade grau I')
+elif imc2 > 35.0 and imc2 < 39.9:
+    print(f'Seu imc {imc2:.2f}, Obesidade grau II (severa)')
+else:
+    imc2 >= 40
+    print(f'Seu imc {imc2:.2f}, Obesidade grau III (mórbida)')
