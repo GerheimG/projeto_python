f9feb209305de74d4c513aacd019d1d3e1612a53
GerheimG <g.gerheimferro@gmail.com>
2025-01-24 15:53:32 -0300

    treino

--- programas_avulsos/matematica/numeros_primos.py
+++ programas_avulsos/matematica/numeros_primos.py
@@ -1,17 +1,16 @@
 import os
 
-
 os.system('cls')
 
-
+# Limite para os números
 valor = int(input("Limite: "))
+
+# Loop para verificar números primos até o valor informado
 for numero in range(2, valor + 1):
-	soma = 0
-	for numero_primo in range(numero):
-		numero_primo += 1
-		if numero % numero_primo == 0:
-			soma += 1
-			if numero_primo == numero and soma <= 2:
-				print(numero_primo, ",", end=" ")
-			elif soma > 2:
-				break
+    soma = 0
+    for numero_primo in range(2, numero):
+        if numero % numero_primo == 0:
+            soma += 1
+            break  # Se houver um divisor, o número não é primo, podemos parar
+    if soma == 0:  # Nenhum divisor encontrado, o número é primo
+        print(numero, ",", end=" ")
