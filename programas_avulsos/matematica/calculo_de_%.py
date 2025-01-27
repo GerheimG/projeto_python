4b97965d0d755369b256c02d05a64fa551c0d1f2
GerheimG <g.gerheimferro@gmail.com>
2025-01-27 17:48:09 -0300

    projeto_python

--- programas_avulsos/matematica/calculo_de_%.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system('cls')
-
-numerox = int(input('Escolha qualquer número inteiro: '))
-porcentagem = int(input('Escolha qual % quer: '))
-
-porcento = numerox * (porcentagem/100)
-
-print(f'Essa é a porcentagem de {numerox}: {porcento:.2f}')
