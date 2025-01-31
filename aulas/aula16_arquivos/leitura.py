b2d1c4baa1f192f96b4e03fa0f6cd158c45f18b4
GerheimG <g.gerheimferro@gmail.com>
2025-01-31 14:50:40 -0300

    aulas

---
+++ aulas/aula16_arquivos/leitura.py
@@ -0,0 +1,36 @@
+import csv
+import os
+
+
+
+# Nome do arquivo CSV
+arquivo = 'arquivos_csv/gravacao/alunas.csv'
+
+
+# Lista para armazenar os dados 
+lista = []
+
+# Abrindo o arquivo CSV para leitura 
+with open(arquivo, 'r') as arquivo_csv:
+    # Criando um leitor de CSV que lê o arquivo como dicionário
+    leitura = csv.DictReader(arquivo_csv, delimiter=';')
+
+    # Iterando sobre cada linha (registro) no arquivo CSV e adicionando à lista 
+    for linha in leitura:
+        lista.append(linha)
+
+os.system('cls')
+
+
+print('-'*60)
+print('nome\ttelefone\tcidade')
+print('-'*60)
+# Exibindo a lista resultante 
+for registro in lista:
+    flag = 0
+    for k, v in registro.items():
+        print(v, end='\t')
+        flag += 1
+        if flag == 3:
+            print()
+print('-'*60)
