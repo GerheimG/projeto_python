e62a35bdbaa0a529507b5765f8687229b9f656dd
GerheimG <g.gerheimferro@gmail.com>
2025-02-19 14:17:26 -0300

    teste

--- treino/teste.py
+++ treino/teste.py
@@ -1,70 +1,80 @@
-estoque = []
-produtos = {}
 import os
-while True:
-    print('-' * 60)
-    pergunta = input('Cadastrar produto? (S/N): ').upper().strip()
-    os.system('cls')
-    if pergunta == 'S':
-        while True:
-            codigo = int(input('Insira o código do produto: '))
-            if any(produtos['Código'] == codigo for produtos in estoque):
-                print('Código indisponivel.')
-                input('Pressione enter...')
-                os.system('cls')
-                continue
-            break
-        while True:
-            nome = input('Insira o nome do produto: ').capitalize().strip()
-            if any(produtos['Nome'] == nome for produtos in estoque):
-                print('Nome indisponivel.')
-                input('Pressione enter...')
-                continue
-            break
-        categoria = input('Insira a categoria do produto: ')
-        while True:
-            try:
-                preco = float(input('Insira o preço do produto: '))
-                if preco < 0:
-                    print('O preço deve ser um valor positivo.Tente novamente.')
-                    continue
-                break
-            except ValueError:
-                print('Valor inválido para preço.Tente novamente.')
-        
-        while True:
-            try:
-                quantidade = int(input('Insira a quantidade de produtos: '))
-                if quantidade < 0:
-                    print('Quantidade não pode ser um valor negativo.'
-                        'Tente novamente')
-                    continue
-                break
-            except ValueError:
-                print('A quantidade deve ser um valor psotivo.Tente novamente.')
-                
+from datetime import datetime
+
+os.system('cls')
+
+class Produto:
+    def __init__(self,nome,preco,categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+        self.lista = []
+        self.dicionario = {
+            'Nome' : nome,
+            'Preço' : preco,
+            'Categoria' : categoria
+        }
+        self.lista.append(self.dicionario)
+        print(self.lista)
     
-        produtos= {
-            'Código': codigo,
-            'Nome': nome,
-            'Categoria': categoria,
-            'Preço': preco,
-            'Quantidade': quantidade
-            }
+    def aplicar_desconto(self):
+        pass
 
-        estoque.append(produtos)
 
-        print("="*50)
-        print("     *** Produto Cadastrado com sucesso ***")
-        print("="*50)
+class Eletronico(Produto):
+    def __init__(self, nome, preco, categoria):
+        super().__init__(nome, preco, categoria)
+        self.desconto = 10
+    
+    def aplicar_desconto(self):
+        return self.preco - (self.preco * self.desconto / 100)
 
-        
-    elif pergunta == 'N':
-        print('Fechando.')
-        break
+class Roupa(Produto):
+    def __init__(self, nome, preco, categoria):
+        super().__init__(nome, preco, categoria)
+        self.desconto_verao = 20
+        self.desconto = 5
+    
+    def aplicar_desconto(self):
+        if self.categoria in self.dicionario['Categoria'] == 'verao':
+            return self.preco - (self.preco * self.desconto_verao / 100)
+        else:
+            return self.preco - (self.preco * self.desconto / 100)
 
-for item in estoque:
-    print(f'Código: {item["Código"]} | Nome: {item["Nome"]} '
-        f'| Categoria: {item["Categoria"]} | Preço: {item["Preço"]} '
-        f'| Quantidade: {item["Quantidade"]}')
-    print('=' * 65)
+class Alimento(Produto):
+    def __init__(self, nome, preco, categoria):
+        super().__init__(nome, preco, categoria)
+        self.desconto = 15
+    
+    def aplicar_desconto(self):
+        return self.preco - (self.preco * self.desconto / 100)
+
+
+
+
+
+# # Teste
+# produto = Eletronico("TV", 1000, "Eletrônicos")
+# print('Nome:', produto.nome)
+# print("Preço original:", produto.preco)
+# print("Preço com desconto:", produto.aplicar_desconto())
+
+# produto1 = Roupa('Blusa', 500, 'Roupa')
+# print('Nome:', produto1.nome)
+# print("Preço original:", produto1.preco)
+# print("Preço com desconto:", produto1.aplicar_desconto())
+
+# produto2 = Alimento('Banana', 40, 'Alimento')
+# print('Nome:', produto2.nome)
+# print("Preço original:", produto2.preco)
+# print("Preço com desconto:", produto2.aplicar_desconto())
+
+nome = input('nome:')
+preco = float(input('preco:'))
+categoria = input('categoria:')
+
+
+produto3 = Roupa(nome,preco,categoria)
+print('Nome:', produto3.nome)
+print("Preço original:", produto3.preco)
+print("Preço com desconto:", produto3.aplicar_desconto())
