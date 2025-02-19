66ded67dcdd506fb77912faacc5b5d903a82e6dc
GerheimG <g.gerheimferro@gmail.com>
2025-02-19 15:31:35 -0300

    atividades

--- atividades/atividade_002_orientado_a_objeto_(herança)/atividade.py
+++ atividades/atividade_002_orientado_a_objeto_(herança)/atividade.py
@@ -19,10 +19,20 @@ class Produto:
         self.nome = nome
         self.preco = preco
         self.categoria = categoria
+        if categoria == 'Roupa':
+            subcategoria = input('Insira a estação (verao ou outras): ')
+            self.subcategoria = subcategoria
+        if categoria == 'Alimento':
+            validade =  input('Insira a data de validade (aaaa/mm/dd): ')
+            validade_obj = datetime.strptime(validade, '%Y/%m/%d')
+            self.validade = validade_obj
         self.lista = []
-        self.lista.append(nome)
-        self.lista.append(preco)
-        self.lista.append(categoria)
+        self.dicionario = {
+            'Nome' : nome,
+            'Preço' : preco,
+            'Categoria' : categoria
+        }
+        self.lista.append(self.dicionario)
         print(self.lista)
     
     def aplicar_desconto(self):
@@ -44,7 +54,11 @@ class Roupa(Produto):
         self.desconto = 5
     
     def aplicar_desconto(self):
-        return self.preco - (self.preco * self.desconto / 100)
+        if self.categoria in self.dicionario['Categoria'] == 'Roupa':
+            if self.subcategoria == 'verao':
+                return self.preco - (self.preco * self.desconto_verao / 100)
+            else:
+                return self.preco - (self.preco * self.desconto / 100)
 
 class Alimento(Produto):
     def __init__(self, nome, preco, categoria):
@@ -52,31 +66,45 @@ class Alimento(Produto):
         self.desconto = 15
     
     def aplicar_desconto(self):
-        return self.preco - (self.preco * self.desconto / 100)
+        self.data_validade = datetime(2032, 3, 25)
+        self.diferenca = self.data_validade - self.validade
+        if self.diferenca.days < 40:
+            print(f'Produto perto da validade... {self.diferenca.days} dias')
+            return self.preco - (self.preco * self.desconto / 100)
+        elif self.diferenca.days > 40:
+            print(f'Produto longe da validade...Preço original mantido. \
+                {self.diferenca.days} dias')
+            return self.preco
 
 
 
 
 
-# Teste
-produto = Eletronico("TV", 1000, "Eletrônicos")
-print('Nome:', produto.nome)
-print("Preço original:", produto.preco)
-print("Preço com desconto:", produto.aplicar_desconto())
+# # Teste
+# produto = Eletronico("TV", 1000, "Eletrônicos")
+# print('Nome:', produto.nome)
+# print("Preço original:", produto.preco)
+# print("Preço com desconto:", produto.aplicar_desconto())
+# print('-'*60)
 
-produto1 = Roupa('Blusa', 500, 'Roupa')
-print('Nome:', produto1.nome)
-print("Preço original:", produto1.preco)
-print("Preço com desconto:", produto1.aplicar_desconto())
+# produto1 = Roupa('Blusa', 500, 'Roupa')
+# print('Nome:', produto1.nome)
+# print("Preço original:", produto1.preco)
+# print("Preço com desconto:", produto1.aplicar_desconto())
+# print('-'*60)
 
-produto2 = Alimento('Banana', 40, 'Alimento')
-print('Nome:', produto2.nome)
-print("Preço original:", produto2.preco)
-print("Preço com desconto:", produto2.aplicar_desconto())
-
-nome = input(':')
-preco = float(input(':'))
-categoria = input(':')
+# produto2 = Alimento('Banana', 40, 'Alimento')
+# print('Nome:', produto2.nome)
+# print("Preço original:", produto2.preco)
+# print("Preço com desconto:", produto2.aplicar_desconto())
 
 
-produto3 = Produto(nome,preco,categoria)
+nome = input('nome:')
+preco = float(input('preco:'))
+categoria = input('categoria:')
+
+
+produto3 = Alimento(nome,preco,categoria)
+print('Nome:', produto3.nome)
+print("Preço original:", produto3.preco)
+print("Preço com desconto:", produto3.aplicar_desconto())
